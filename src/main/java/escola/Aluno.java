0a501d5f99b52d7c75d5885238f9a16f047c2d8a
guilhermeclamego <guilhermeclamego@gmail.com>
2021-02-16 17:47:16 -0300

    Criacao de objetos

---
+++ src/main/java/escola/Aluno.java
@@ -0,0 +1,11 @@
+package escola;
+
+public class Aluno {
+
+    //Entidade alunos é unico
+    private CPF cpf;
+    private String nome;
+    private Email email;
+
+
+}
---
+++ src/main/java/escola/Email.java
@@ -0,0 +1,16 @@
+package escola;
+
+public class Email {
+
+    //Value Object poder ter o mesmo valor para os objetos
+    private String endereco;
+
+    public Email(String endereco) {
+        if(endereco == null || !endereco.matches("^[\\\\w\\\\.-]+@([\\\\w\\\\-]+\\\\.)+[A-Z]{2,4}$\"")){
+            throw new IllegalArgumentException("Endereço de e-mail inválido!");
+        }
+        this.endereco = endereco;
+    }
+
+
+}
