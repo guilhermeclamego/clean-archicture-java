6b34669f7d3e500cba25531a5ef56933d7990fc8
guilhermeclamego <guilhermeclamego@gmail.com>
2021-02-17 18:29:34 -0300

    Criacao de service para testes

--- src/main/java/br/com/alura/escola/dominio/aluno/CPF.java
+++ src/main/java/br/com/alura/escola/dominio/aluno/CPF.java
@@ -5,8 +5,9 @@ public class CPF {
     private String numero;
 
     public CPF(String numero) {
-        if(numero == null || !numero.matches("/^\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}$/")){
-            throw new IllegalArgumentException("CPF Inválido!");
+        if (numero == null ||
+                !numero.matches("\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")) {
+            throw new IllegalArgumentException("CPF invalido!");
         }
         this.numero = numero;
     }
--- src/main/java/br/com/alura/escola/dominio/aluno/Email.java
+++ src/main/java/br/com/alura/escola/dominio/aluno/Email.java
@@ -6,9 +6,11 @@ public class Email {
     private String endereco;
 
     public Email(String endereco) {
-        if(endereco == null || !endereco.matches("^[\\\\w\\\\.-]+@([\\\\w\\\\-]+\\\\.)+[A-Z]{2,4}$\"")){
-            throw new IllegalArgumentException("Endereço de e-mail inválido!");
+        if (endereco == null ||
+                !endereco.matches("^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")) {
+            throw new IllegalArgumentException("E-mail invalido!");
         }
+
         this.endereco = endereco;
     }
 
