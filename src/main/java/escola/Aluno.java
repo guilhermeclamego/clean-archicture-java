3b435fc9fc2dfc2fa12be420655b05178ec2a19d
guilhermeclamego <guilhermeclamego@gmail.com>
2021-02-16 21:59:38 -0300

    Teste Unitários

--- src/main/java/escola/Aluno.java
+++ src/main/java/escola/Aluno.java
@@ -1,12 +1,19 @@
 package escola;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aluno {
 
     //Entidade alunos é unico
     private CPF cpf;
     private String nome;
     private Email email;
-    private Telefone telefone;
+    private List<Telefone> telefones = new ArrayList<>();
+
+    public void adicionarTelefone(String ddd, String numero){
+        this.telefones.add(new Telefone(ddd, numero));
+    }
 
 
 }
--- src/main/java/escola/Telefone.java
+++ src/main/java/escola/Telefone.java
@@ -1,4 +1,18 @@
 package escola;
 
 public class Telefone {
+
+    private String ddd;
+    private String numero;
+
+    public Telefone(String ddd, String numero) {
+        if(ddd == null || !ddd.matches("^\\([1-9]{2}\\)")){
+            throw new IllegalArgumentException("DDD Inválido");
+        }
+        if(numero == null || !numero.matches("/^\\d{5}\\-\\d{4}$/")){
+            throw new IllegalArgumentException("Número inválido");
+        }
+        this.ddd = ddd;
+        this.numero = numero;
+    }
 }
