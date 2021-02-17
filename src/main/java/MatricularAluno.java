bc87fbac1d54c5a41c621eed909f6f3079fec644
guilhermeclamego <guilhermeclamego@gmail.com>
2021-02-17 18:20:35 -0300

    Criacao de Services

---
+++ src/main/java/MatricularAluno.java
@@ -0,0 +1,3 @@
+public class MatricularAluno {
+
+}
---
+++ src/main/java/br/com/alura/escola/aplicacao/indicacao/EnviarEMailIndicacao.java
@@ -0,0 +1,7 @@
+package br.com.alura.escola.aplicacao.indicacao;
+
+import br.com.alura.escola.dominio.aluno.Aluno;
+
+public interface EnviarEMailIndicacao {
+    void enviarPara(Aluno indicado);
+}
